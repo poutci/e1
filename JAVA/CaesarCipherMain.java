d8886a00d102ccabf9be48aec32862a15c3b9b60
poutci <61775170+poutci@users.noreply.github.com>
2020-10-08 23:19:00 -0400

    comments

--- JAVA/CaesarCipherMain.java
+++ JAVA/CaesarCipherMain.java
@@ -12,32 +12,40 @@ public class CaesarCipherMain {
 	 
  }// 12 operations or O(1)
   static class CaesarCipher{
+	  //sets up the encoder and decoder arrays
 	  private char[] encoder = new char[52]; //1
 	  private char[] decoder = new char[52]; //1
 	  
 	  public CaesarCipher(int rotation) {
+		  //for uppercase rotation
 		  for (int k = 0; k < 26; k++) { //26
 			  encoder[k] = (char)('A'+(k+rotation)%26); // 5
 			  decoder [k] = (char)('A'+(k-rotation+26) %26); //6
 		  } // 37
+		  //for lower case rotation
 		  for(int j = 26; j < 52; j++) { // 26
 			  encoder[j] = (char)('a'+(j+rotation)%26); //5
 			  decoder[j] = (char)('a'+(j-rotation+26)%26); //6
 		  }//37
 	  }// 74 operations or O(1)
+	  //encrypts the string
 	  public String encrypt(String m) {
 		  return transform(m, encoder); //1 + O(n)
 	  } // 5n + 2 operations or 6n + 2 operations or O(n)
+	  //decrypts the string
 	  public String decrypt(String m) {
 		  return transform(m, decoder); // 1 + O(n)
 	  }// 5n + 2 operations or 6n + 2 operations or O(n)
+	  //used to either encrpty or decrypt with string with the given rotation
 	  private String transform(String o, char[] code) {
 		  char[] msg = o.toCharArray(); //1
 		  for(int i=0; i<msg.length; i++) { //n 
+			  // used for uppercase letters
 			  if(Character.isUpperCase(msg[i])) { // 1
 				  int j = msg[i] - 'A'; //2
 				  msg[i] = code[j]; // 1
 			  }
+			  //used for lowercase letters
 			  else if(Character.isLowerCase(msg[i])){ //1
 				  int l = msg[i] - 'a'; //2
 				  msg[i] = code[26+l]; //2
